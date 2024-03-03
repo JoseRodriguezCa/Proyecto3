a5ec6b7368c76b0b32f18a1e988d8ff597673526
Jose Rodriguez <128047100+JoseRodriguezCa@users.noreply.github.com>
2024-03-03 23:03:04 +0100

    1

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,8 +5,11 @@ import './Logo.css'
 export const logo = () => {
     const logoContainer = document.createElement('div')
     const imgLogo = document.createElement('img')
+    const aLogo = document.createElement('a')
+    aLogo.href = '/'
     imgLogo.src = 'https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png'
     logoContainer.classList = 'logo'
-    logoContainer.append(imgLogo)
+    aLogo.append(imgLogo)
+    logoContainer.append(aLogo)
     return logoContainer
 }
