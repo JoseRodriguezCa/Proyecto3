da3d3050bd4de2bcac18bc1ace72d6ad467a5c1b
Jose Rodriguez <128047100+JoseRodriguezCa@users.noreply.github.com>
2024-03-04 21:33:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,22 +3,36 @@ import { header } from './src/components/Header/Header';
 import { pintarCarta } from './src/components/Carta/Carta'
 import { buscar } from './src/components/Buscador/Buscador';
 
-
+let solicitud = false;
 let sumPage = 1;
-const h = header()
-document.body.append(h)
-const buscador = document.querySelector('.buscador')
-const busca = buscar(sumPage)
-const handleScroll = () => {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        sumPage ++;
-        pintarCarta(sumPage)
-        console.log(buscador.value)
+const h = header();
+document.body.append(h);
+const buscador = document.querySelector('.buscador');
+buscar(sumPage);
+
+const cargarContenido = () => {
+    if (!solicitud && (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        sumPage++;
+        pintarCarta(buscador.value, sumPage);
+        console.log(buscador.value);
+        solicitud = true;
     }
 }
+
+const handleScroll = () => {
+    cargarContenido();
+    setTimeout(() => {
+        solicitud = false;
+    }, 2000);
+}
+
 window.addEventListener('scroll', handleScroll);
-const carta = pintarCarta(sumPage)
+const cartas = async () => {
+const carta = await pintarCarta(buscador.value, sumPage)
 document.body.append(carta);
+}
+
+cartas()
 
 
 
